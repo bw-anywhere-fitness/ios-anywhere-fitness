bc10eca5316fedd41823d4497fede109872793f8
Michael R.F <mrflowers22@uwalumni.com>
2019-05-20 16:22:01 -0400

    connected the outlets and actions to the source code.

--- Anywhere Fitness/Anywhere Fitness/SignUpViewController.swift
+++ Anywhere Fitness/Anywhere Fitness/SignUpViewController.swift
@@ -10,13 +10,39 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    //MARK: - LABEL OUTLETS
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var confirmPasswordLabel: UILabel!
+    @IBOutlet weak var fitnessInstrLabel: UILabel!
+    
+    //MARK: - More Outlets
+    @IBOutlet weak var switchProperties: UISwitch!
+    @IBOutlet weak var segmentedProperties: UISegmentedControl!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //MARK: - TEXTFIELD OUTLETS
+    @IBOutlet weak var firstNameTF: UITextField!
+    @IBOutlet weak var lastNameTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var confirmPasswordTF: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
+    //MARK: IBActions
+    @IBAction func switchValue(_ sender: UISwitch) {
+    }
+    
+    @IBAction func segmentedValue(_ sender: UISegmentedControl) {
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -27,4 +53,10 @@ class SignUpViewController: UIViewController {
     }
     */
 
+    //Mark: UI Functions
+    func firstView(){
+        //after the user downloads the app and opens it up for the first time, the screen should be blank until user hits an option on the segmented control
+        
+    }
+    
 }
