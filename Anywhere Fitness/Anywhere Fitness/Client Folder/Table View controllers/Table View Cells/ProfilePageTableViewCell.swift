cb5da18fd7ed75b0ef5fb93fc59e17797a7b47a4
Michael R.F <mrflowers22@uwalumni.com>
2019-05-22 10:06:06 -0400

    assigned source files to applicable storyboard scenes.

---
+++ Anywhere Fitness/Anywhere Fitness/Client Folder/Table View controllers/Table View Cells/ProfilePageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfilePageTableViewCell.swift
+//  Anywhere Fitness
+//
+//  Created by Michael Flowers on 5/22/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Anywhere Fitness/Anywhere Fitness/Client Folder/Table View controllers/WorkoutListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WorkoutListCollectionViewCell.swift
+//  Anywhere Fitness
+//
+//  Created by Michael Flowers on 5/22/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutListCollectionViewCell: UICollectionViewCell {
+    
+}
