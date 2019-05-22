cb707a960fdadd1cedd516c0b4bf847e14c4ed1f
Michael R.F <mrflowers22@uwalumni.com>
2019-05-22 09:23:23 -0400

    made bearer struct to hold the token

---
+++ Anywhere Fitness/Anywhere Fitness/Model Controller/ClientController.swift
@@ -0,0 +1,16 @@
+//
+//  ClientController.swift
+//  Anywhere Fitness
+//
+//  Created by Michael Flowers on 5/22/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+class ClientController {
+    //get the base url
+    private let baseURL = URL(string: "https://anywhere-fitness.herokuapp.com/")!
+    
+    //post to get auth
+}
